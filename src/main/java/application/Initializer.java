04aa1623f9663e24fb8d75a12cda9cc6353e12e6
ryteck <rya_gm@hotmail.com>
2019-06-19 18:01:57 -0300

    Inserção de icones no projeto

--- src/main/java/application/Initializer.java
+++ src/main/java/application/Initializer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Initializer extends Application {
@@ -20,6 +21,7 @@ public class Initializer extends Application {
         Scene scene = new Scene(root);
 
         stage.setTitle("Conversor");
+        stage.getIcons().add(new Image("/img/num.png"));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
